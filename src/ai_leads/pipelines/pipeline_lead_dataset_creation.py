e814a8f209363385ca7b78ad632c44a331bf7177
Raphaël Adda <raphaeladda@macbook-air.home>
2023-11-04 22:07:45 +0100

    add evthg

--- src/ai_leads/pipelines/pipeline_lead_dataset_creation.py
+++ src/ai_leads/pipelines/pipeline_lead_dataset_creation.py
@@ -5,10 +5,8 @@ from ai_leads.model.lead_dataset_creation import LeadDataFrameConverter
 # Setup logging
 logger = logging.getLogger(__name__)
 
+
 df_jobs = pd.read_csv("src/ai_leads/jobs_tests.csv", sep=";")
-
 dfConverter = LeadDataFrameConverter(df_jobs)
-
 df_leads = dfConverter.convert_to_lead_dataframe(time_window=10)
-
 df_leads.to_csv("src/ai_leads/leads_tests.csv", sep=",")
