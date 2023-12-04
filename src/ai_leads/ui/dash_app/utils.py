ce52bf0ed623a96bcafa2ea81a1bbfa68afd8a79
Raphaël Adda <raphaeladda@MacBook-Air-2.local>
2023-12-04 18:56:36 +0100

    add evth

--- src/ai_leads/ui/dash_app/utils.py
+++ src/ai_leads/ui/dash_app/utils.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import os
 import numpy as np
+from unidecode import unidecode
+from ai_leads.Config.param import LEAD_FILE_PATH
 
 # Data
 DATA_PATH = "data/"
-df_final_result_leads = pd.read_csv(os.path.join(DATA_PATH, "leads_tests.csv"), sep=",")
+df_final_result_leads = pd.read_csv(os.path.join(LEAD_FILE_PATH), sep=",")
 # df_final_result_uni["segment"] = "university"
 df_final_result_leads.fillna(np.nan, inplace=True)
-df_final_result_leads["company_id"] = df_final_result_leads["company_id"].astype(str)
+df_final_result_leads["Entreprise"] = df_final_result_leads["Entreprise"].astype(str)
 
 
 def extract_client_name(path: str) -> str:
-    id = path[len("/list_offers/") :]
-    df_final_result_leads[df_final_result_leads["company_id"] == str(id)]
-    company = df_final_result_leads[df_final_result_leads["company_id"] == str(id)]["Entreprise"].iloc[0]
+    unidecoded_company = path[len("/list_offers/") :]
+    company = df_final_result_leads[
+        df_final_result_leads["Entreprise"].apply(lambda x: unidecode(x).replace(" ", "")) == unidecoded_company
+    ]["Entreprise"].iloc[0]
     return company
