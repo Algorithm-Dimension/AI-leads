39dc1033a3b5116533e444e5a5c51bc84e6f7d0e
Raphaël Adda <raphaeladda@macbook-air.home>
2024-02-06 22:37:32 +0100

    minor change

--- src/ai_leads/model/navigator.py
+++ src/ai_leads/model/navigator.py
@@ -76,7 +76,6 @@ class WebpageScraper:
             "Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36",
         ]
         import random
-        from selenium.webdriver.chrome.options import Options
 
         user_agent = random.choice(user_agents)
 
